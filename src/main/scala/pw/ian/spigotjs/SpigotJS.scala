bc0e44b9fbfde92fb104572f5f2b80d2be413206
Ian Macalinao <me@ian.pw>
2015-11-25 11:53:57 -0600

    Make scripts directory work

--- src/main/scala/pw/ian/spigotjs/SpigotJS.scala
+++ src/main/scala/pw/ian/spigotjs/SpigotJS.scala
@@ -1,5 +1,6 @@
 package pw.ian.spigotjs
 
+import java.io.InputStreamReader
 import javax.script.ScriptEngineManager
 
 import org.bukkit.plugin.java.JavaPlugin
@@ -8,10 +9,8 @@ class SpigotJS extends JavaPlugin {
 
   override def onEnable() = {
     val engine = new ScriptEngineManager().getEngineByName("nashorn")
-  }
-
-  override def onDisable() = {
-
+    engine.eval(new InputStreamReader(this.getResource("jvm-npm.js")))
+    engine.eval("require('./scripts')")
   }
 
 }
